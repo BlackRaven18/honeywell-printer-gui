26992b601afa6338d9ffd63b988a78f69f62ecd3
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2023-07-14 14:21:57 +0200

    first commit

---
+++ WpfApp2/LablelStringToPrinterClass.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIVEVMSLabels
+{
+    public class LablelStringToPrinter
+    {
+
+        public static string ShelfNo4(string rack, string xcoor, string ycoor, string cheksum)
+        {
+            string FirstTree = "";
+            string SecondTree = "";
+
+
+            FirstTree = String.Join(Environment.NewLine,
+                                                            "ALIGN 1",
+
+                                                            "CTRLSUM$ = \"" + cheksum + "\"",
+
+                                                            "SHELF$= \"" + ycoor.Substring(1, 1) + "\"",
+
+                                                            "CORENUM1$ = \"" + rack + "\"",
+                                                            "CORENUM2$ = \"" + xcoor + "\"",
+                                                            "CORENUM3$ = \"" + ycoor + "\"",
+
+                                                            "PRPOS60, 160",
+                                                            "BARFONT OFF",
+                                                            "BARSET \"CODE128C\", 3, 1, 10, 140, 0, 0",
+                                                            "PRBAR CTRLSUM$",
+
+                                                            "FONT \"Univers Bold\", 48",
+                                                            "PRPOS30, 01",
+                                                            "PRTXT CTRLSUM$",
+
+                                                            "DIR 1",
+                                                            "FONT \"Univers Bold\", 36",
+                                                            "PRPOS55, 325",
+                                                            "PRTXT SHELF$",
+
+                                                            "NORIMAGE",
+                                                            "PRPOS25, 325",
+                                                            "PRBOX125, 125, 15",
+
+                                                            "PRPOS 180, 425 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+                                                            "PRPOS 180, 390 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+                                                            "PRPOS 180, 355 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+                                                            "PRPOS 180, 320 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\""
+                                                );
+
+
+            if(int.Parse(ycoor) >= 5)
+            {
+                SecondTree = String.Join(Environment.NewLine,
+                                                            "PRPOS 300, 320 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\""
+                                        );
+            }
+            if (int.Parse(ycoor) >= 6)
+            {
+                SecondTree = String.Join(Environment.NewLine,
+                                                            SecondTree,
+                                                            "PRPOS 300, 355 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\""
+                                        );
+            }
+            if (int.Parse(ycoor) >= 7)
+            {
+                SecondTree = String.Join(Environment.NewLine,
+                                                            SecondTree,
+                                                            "PRPOS 300, 390 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\""
+                                        );
+            }
+            if (int.Parse(ycoor) >= 8)
+            {
+                SecondTree = String.Join(Environment.NewLine,
+                                                            SecondTree,
+                                                            "PRPOS 300, 425 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\""
+                                        );
+            }
+
+
+
+            return (String.Join(Environment.NewLine,
+                                                            FirstTree,
+                                                            SecondTree,
+                                                            "PRINTFEED",
+                                                            "NEXT",
+                                                            ""
+
+                                        ));
+        }
+
+
+        public static string ShelfNo3(string rack, string xcoor, string cheksum)
+        {
+            return (String.Join(Environment.NewLine,
+                                                            "ALIGN 1",
+
+                                                            "CTRLSUM$ = \"" + cheksum + "\"",
+
+                                                            "SHELF$= \"3\"",
+
+                                                            "CORENUM1$ = \"" + rack + "\"",
+                                                            "CORENUM2$ = \"" + xcoor + "\"",
+                                                            "CORENUM3$ = \"0\" + SHELF$",
+
+                                                            "PRPOS60, 160",
+                                                            "BARFONT OFF",
+                                                            "BARSET \"CODE128C\", 3, 1, 10, 140, 0, 0",
+                                                            "PRBAR CTRLSUM$",
+
+                                                            "FONT \"Univers Bold\", 48",
+                                                            "PRPOS30, 01",
+                                                            "PRTXT CTRLSUM$",
+
+                                                            "DIR 1",
+                                                            "FONT \"Univers Bold\", 36",
+                                                            "PRPOS55, 325",
+                                                            "PRTXT SHELF$",
+
+                                                            "NORIMAGE",
+                                                            "PRPOS25, 325",
+                                                            "PRBOX125, 125, 15",
+
+                                                            "PRPOS 180, 420 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+                                                            "PRPOS 180, 375 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+                                                            "PRPOS 180, 330 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+
+                                                            "PRINTFEED",
+                                                            "NEXT",
+                                                            ""
+
+                                        ));
+        }
+
+        public static string ShelfNo2(string rack, string xcoor, string cheksum)
+        {
+            return (String.Join(Environment.NewLine,
+                                                            "ALIGN 1",
+
+                                                            "CTRLSUM$ = \"" + cheksum + "\"",
+
+                                                            "SHELF$= \"2\"",
+
+                                                            "CORENUM1$ = \"" + rack + "\"",
+                                                            "CORENUM2$ = \"" + xcoor + "\"",
+                                                            "CORENUM3$ = \"0\" + SHELF$",
+
+                                                            "PRPOS60, 160",
+                                                            "BARFONT OFF",
+                                                            "BARSET \"CODE128C\", 3, 1, 10, 140, 0, 0",
+                                                            "PRBAR CTRLSUM$",
+
+                                                            "FONT \"Univers Bold\", 48",
+                                                            "PRPOS30, 01",
+                                                            "PRTXT CTRLSUM$",
+
+                                                            "DIR 1",
+                                                            "FONT \"Univers Bold\", 36",
+                                                            "PRPOS55, 325",
+                                                            "PRTXT SHELF$",
+
+                                                            "NORIMAGE",
+                                                            "PRPOS25, 325",
+                                                            "PRBOX125, 125, 15",
+
+                                                            "PRPOS 180, 375 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+                                                            "PRPOS 180, 330 - 10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+
+                                                            "PRINTFEED",
+                                                            "NEXT"
+
+                                        ));
+        }
+
+
+        public static string ShelfNo1(string rack, string xcoor, string cheksum)
+        {
+            return (String.Join(Environment.NewLine,
+                                                            "ALIGN 1",
+
+                                                            "CTRLSUM$ = \"" + cheksum + "\"",
+
+                                                            "SHELF$= \"1\"",
+
+                                                            "CORENUM1$ = \"" + rack + "\"",
+                                                            "CORENUM2$ = \"" + xcoor + "\"",
+                                                            "CORENUM3$ = \"0\" + SHELF$",
+
+                                                            "PRPOS60, 160",
+                                                            "BARFONT OFF",
+                                                            "BARSET \"CODE128C\", 3, 1, 10, 140, 0, 0",
+                                                            "PRBAR CTRLSUM$",
+
+                                                            "FONT \"Univers Bold\", 48",
+                                                            "PRPOS30, 01",
+                                                            "PRTXT CTRLSUM$",
+
+                                                            "DIR 1",
+                                                            "FONT \"Univers Bold\", 36",
+                                                            "PRPOS55, 325",
+                                                            "PRTXT SHELF$",
+
+                                                            "NORIMAGE",
+                                                            "PRPOS25, 325",
+                                                            "PRBOX125, 125, 15",
+
+                                                            "PRPOS 180, 330-10",
+                                                            "PRIMAGE \"TRIANGLE.1\"",
+
+                                                            "PRINTFEED",
+                                                            "NEXT"
+
+                                        ));
+        }
+
+
+        public static string ShelfNo0(string rack, string xcoor, string cheksum)
+        {
+            return (String.Join(Environment.NewLine,
+                                                            "ALIGN 1",
+
+                                                            "CTRLSUM$ = \"" + cheksum + "\"",
+
+                                                            "SHELF$= \"" + rack + "\"",
+
+                                                            //"CORENUM1$ = \"" + rack + "\"",
+                                                            //"CORENUM2$ = \"" + xcoor + "\"",
+                                                            //"CORENUM3$ = \"0\" + SHELF$",
+
+                                                            "PRPOS60, 160",
+                                                            "BARFONT OFF",
+                                                            "BARSET \"CODE128\", 3, 1, 4, 120, 0, 0",
+                                                            "PRBAR CTRLSUM$",
+
+                                                            "FONT \"Univers Bold\", 40",
+                                                            "PRPOS30, 01",
+                                                            "PRTXT CTRLSUM$",
+
+                                                            //"DIR 3",
+                                                            //"PRPOS265, 380",
+                                                            //"PRIMAGE \"TRIANGLE.1\"",
+
+                                                            "DIR 1",
+                                                            //"INVIMAGE",
+                                                            "FONT \"Univers Bold\", 40",
+                                                            "PRPOS50, 325",
+                                                            "PRTXT SHELF$",
+
+                                                            //"NORIMAGE",
+                                                            //"PRPOS30, 325",
+                                                            //"PRBOX125, 99, 20",
+
+                                                            "PRINTFEED",
+                                                            "NEXT"
+
+                                        ));
+        }
+
+
+        public static string StringToPrinter(DataRow OneRow)
+        {
+            string thisstring;
+            string rack, xcoor, ycoor, cheksum;
+
+            rack = "01";
+            xcoor = "01";
+            ycoor = "01";
+            //rack = OneRow[1].ToString();
+            //xcoor = OneRow[2].ToString();
+            //ycoor = OneRow[3].ToString();
+            cheksum = OneRow[3].ToString();
+
+            thisstring = ShelfNo0(cheksum.Substring(7, 1), xcoor, cheksum);
+
+            /**
+            switch (ycoor)
+            {
+                case "00":
+                    thisstring = ShelfNo0(rack, xcoor, cheksum);
+                    break;
+                case "01":
+                    thisstring = ShelfNo1(rack, xcoor, cheksum);
+                    break;
+                case "02":
+                    thisstring = ShelfNo2(rack, xcoor, cheksum);
+                    break;
+                case "03":
+                    thisstring = ShelfNo3(rack, xcoor, cheksum);
+                    break;
+                default:
+                    thisstring = ShelfNo4(rack, xcoor, ycoor, cheksum);
+                    break;
+            }
+            **/
+
+            //Debug.WriteLine(thisstring);
+            return thisstring;
+        }
+    }
+
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using System.Data;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows.Media;
+
+namespace VIVEVMSLabels
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void UpdatetxtInfo1(string newtxt, SolidColorBrush brush)
+        {
+            txtInfo1.Content = newtxt;
+            txtInfo1.Foreground = brush;
+        }
+
+        private void UpdatedtGridItemsSource()
+        {
+            dtGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private delegate void Updater(string udnewtxt, SolidColorBrush udbrush);
+
+        private delegate void UpdaterGrid();
+
+        protected DataTable dt;
+
+        private void ReadLabels(string chosenFile)
+        {
+            Updater uiUpdater = new Updater(UpdatetxtInfo1);
+
+            Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, $"Czekaj trwa wczytywanie pozycji.", Brushes.Red);
+
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                //Static File From Base Path...........
+                //Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "TestExcel.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                //Dynamic File Using Uploader...........
+                Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(chosenFile, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                Microsoft.Office.Interop.Excel.Worksheet excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelBook.Worksheets.get_Item(1); ;
+                Microsoft.Office.Interop.Excel.Range excelRange = excelSheet.UsedRange;
+
+
+
+                string strCellData = "";
+                double douCellData;
+                int rowCnt = 0;
+                int colCnt = 0;
+
+                dt = new DataTable();
+                for (colCnt = 1; colCnt <= excelRange.Columns.Count; colCnt++)
+                {
+                    string strColumn = "";
+                    strColumn = (string)(excelRange.Cells[1, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                    strColumn = strColumn.Trim();
+                    dt.Columns.Add(strColumn, typeof(string));
+                }
+
+
+                int rowcount = excelRange.Rows.Count - 1;
+
+                for (rowCnt = 2; rowCnt <= excelRange.Rows.Count; rowCnt++)
+                {
+                    Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, $"Czekaj trwa wczytywanie pozycji {rowCnt-1} / {rowcount}.", Brushes.Red);
+                    string strData = "";
+                    for (colCnt = 1; colCnt <= excelRange.Columns.Count; colCnt++)
+                    {
+                        try
+                        {
+                            strCellData = (string)(excelRange.Cells[rowCnt, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                            strCellData = strCellData.Trim();
+                            strData += strCellData + "|";
+                        }
+                        catch (Exception ex)
+                        {
+                            douCellData = (excelRange.Cells[rowCnt, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                            strData += douCellData.ToString() + "|";
+                        }
+                    }
+                    strData = strData.Remove(strData.Length - 1, 1);
+                    dt.Rows.Add(strData.Split('|'));
+                }
+
+            UpdaterGrid uiUpdaterGrid = new UpdaterGrid(UpdatedtGridItemsSource);
+            Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdaterGrid);
+
+            excelBook.Close(true, null, null);
+            excelApp.Quit();
+            
+
+            Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, $"Wybierz akcję ({dt.Rows.Count} pozycji.)", Brushes.Black);
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.DefaultExt = ".xlsx";
+            openfile.Filter = "(.xlsx)|*.xlsx";
+            //openfile.ShowDialog();
+
+            var browsefile = openfile.ShowDialog();
+
+            txtFilePath.Text = openfile.FileName;
+            string chosenFile;
+
+            chosenFile = txtFilePath.Text.ToString();
+
+
+            if (browsefile == true)
+            {
+                
+                Thread t = new Thread(() => ReadLabels(chosenFile));
+                t.Start();
+                //t.Join();
+               
+            }
+
+        }
+
+        private void PrintLabels(string selectedport)
+        {
+            Updater uiUpdater = new Updater(UpdatetxtInfo1);
+            Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, "Trwa drukowanie", Brushes.Red);
+
+
+            SerialPort MyCOMPort = new SerialPort(); // Create a new SerialPort Object
+
+            //COM port settings to 8N1 mode 
+            //MyCOMPort.PortName = "COM9";            // Name of the COM port 
+            MyCOMPort.PortName = selectedport;              // Name of the COM port 
+            MyCOMPort.BaudRate = 9600;               // Baudrate = 9600bps
+            MyCOMPort.Parity = Parity.None;          // Parity bits = none  
+            MyCOMPort.DataBits = 8;                  // No of Data bits = 8
+            MyCOMPort.StopBits = StopBits.One;       // No of Stop bits = 1
+
+            MyCOMPort.Open();
+
+
+            int printnumber;
+            printnumber = 1;
+
+            foreach (DataRow OneRow in dt.Rows)
+            {
+
+                //RawPrinterHelper.SendStringToPrinter("Generic / Text Only", LablelStringToPrinter.StringToPrinter(OneRow));
+
+                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, $"Trwa drukowanie {printnumber++} / {dt.Rows.Count} " , Brushes.Red);
+                MyCOMPort.Write(LablelStringToPrinter.StringToPrinter(OneRow));
+                System.Threading.Thread.Sleep(1500);
+                //Debug.WriteLine(OneRow[3]);
+                //foreach (var item in OneRow.ItemArray)
+                //{
+                //    Debug.WriteLine(item);
+                //}
+            }
+
+            MyCOMPort.Close();
+
+            //using System.Threading.Tasks;
+            /*
+            Task.Run(() =>
+            {
+                Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    txtInfo1.Content = "Wybierz akcję.";
+                    txtInfo1.Foreground = Brushes.Black;
+                }), DispatcherPriority.Render);
+                //Thread.Sleep(1);
+            });
+            */
+            Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, $"Wybierz akcję ({dt.Rows.Count} pozycji).", Brushes.Black);
+        }
+
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+
+            string selectedport = (string)comboboxforcom.SelectedValue;
+
+            Thread t = new Thread(() => PrintLabels(selectedport));
+            t.Start();
+
+        }
+
+        private void Cmb_Initialized(object sender, EventArgs e)
+        {
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                comboboxforcom.Items.Add(s);
+            }
+        }
+
+        private void Cmb_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            btnPrint.IsEnabled = true;
+
+            SerialPort MyCOMPort = new SerialPort();
+            MyCOMPort.PortName = (string)comboboxforcom.SelectedValue;            // Name of the COM port 
+            MyCOMPort.BaudRate = 9600;               // Baudrate = 9600bps
+            MyCOMPort.Parity = Parity.None;          // Parity bits = none  
+            MyCOMPort.DataBits = 8;                  // No of Data bits = 8
+            MyCOMPort.StopBits = StopBits.One;       // No of Stop bits = 1
+            MyCOMPort.Open();
+            MyCOMPort.Write(String.Join(Environment.NewLine,
+                                                            "ALIGN 1",
+
+                                                            "FONT \"Univers Bold\", 20",
+                                                            "PRPOS10, 30",
+                                                            "PRTXT \"" + (string)comboboxforcom.SelectedValue + "\"",
+
+                                                            "PRINTFEED",
+                                                            "NEXT"
+
+                                        ));
+            MyCOMPort.Close();
+
+        }
+    }
+}
+
